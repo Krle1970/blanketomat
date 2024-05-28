2efe7f3476d2f00a6d08913e61bc67f6189c9b0e
Strahinja <strahinja.velickovic@elfak.rs>
2024-05-28 23:12:31 +0200

    Dodata pocetna strana i filtriranje

---
+++ Aplikacija/Blanketomat/Front/PageStudente/skripte/script.js
@@ -0,0 +1,19 @@
+var toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("expand");
+
+    
+    var icon = toggleBtn.querySelector('i');
+
+    
+    if (icon.classList.contains('lni-angle-double-right')) {
+        
+        icon.classList.remove('lni-angle-double-right');
+        icon.classList.add('lni-angle-double-left');
+    } else {
+        
+        icon.classList.remove('lni-angle-double-left');
+        icon.classList.add('lni-angle-double-right');
+    }
+});
