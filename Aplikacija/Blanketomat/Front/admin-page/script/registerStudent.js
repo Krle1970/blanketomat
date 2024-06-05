bb9662d2220ca440fab0621a3280599394139dfb
Stefan <stefanmilic@elfak.rs>
2024-06-05 23:48:39 +0200

    dodata registracija za asistenta i studenta

--- Aplikacija/Blanketomat/Front/admin-page/script/registerStudent.js
+++ Aplikacija/Blanketomat/Front/admin-page/script/registerStudent.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const ime = document.getElementById('name1');
-    const prezime = document.getElementById('surname1');
-    const email = document.getElementById('email1');
-    const lozinka = document.getElementById('password1');
-    const akreditacijaId = document.getElementById('akreditacijaId1');
-    const smerId = document.getElementById('smerId1');
-    const predmetiIds = document.getElementById('predmetiIds1');
-    const dugme = document.getElementById('submitProfesor');
+    const ime = document.getElementById('addName');
+    const prezime = document.getElementById('addSurname');
+    const email = document.getElementById('addEmail');
+    const lozinka = document.getElementById('addPassword');
+    const akreditacijaId = document.getElementById('addAkreditacija');
+    const smer = document.getElementById('addSmer');
+    const predmet = document.getElementById('addPredmet');
+    
+    const dugme = document.getElementById('submitStudent');
 
-    async function registrujProfesora(name, surname, mail, pass) {
+    async function registrujAsistenta(name, surname, mail, pass) {
         const body = {
             Ime: name,
             Prezime: surname,
             Email: mail,
             Password: pass,
             AkreditacijaId: akreditacijaId ? parseInt(akreditacijaId.value) : null,
-            SmerId: smerId ? parseInt(smerId.value) : null,
-            PredmetiIds: predmetiIds.value.split(',').map(id => parseInt(id.trim()))
+            Smer: smer,
+            Predmet: predmet
         };
 
         const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbmlzdHJhdG9yIiwiZXhwIjoxNzE3Njk2NjAzfQ.ltK8C5EjwQl2dygYH_xiwuposDaHvgq2mif_xYbW4dmXiXcf2DOH3AFQDog1wl3gPPjDsvw9mNjlH4VBKUAHSQ'; 
-        console.log(token);
+        //console.log(token);
         try {
-            const response = await fetch('http://localhost:5246/Auth/register-profesor', {
+            const response = await fetch('http://localhost:5246/Auth/register-student', {
                 method: 'POST',
                 body: JSON.stringify(body),
                 headers: {
@@ -42,13 +43,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         } catch (error) {
             console.error('Error:', error);
-            alert('Došlo je do greške pri registraciji');
+            //alert('Došlo je do greške pri registraciji');
         }
     }
 
     dugme.addEventListener('click', function(event) {
         event.preventDefault();
-        registrujProfesora(
+        registrujStudenta(
             ime.value, prezime.value, email.value, lozinka.value
         );
     });
